fbd5bdf7bed5dbcd7f38839ebb571eba87ff241c
fengyu <fengyu@raiyee.com>
2020-08-05 10:23:51 +0800

    添加 fastdfs 配置文件

--- src/main/java/rain/file/FastDFSClient.java
+++ src/main/java/rain/file/FastDFSClient.java
@@ -10,6 +10,23 @@ import java.io.File;
 
 @Slf4j
 @Component
+/**
+ * 使用 FastDFS 的准备工作
+ * 1、添加 fast-dfs 依赖 serach.maven 里面下最新的
+ *         <dependency>
+ *             <groupId>org.csource</groupId>
+ *             <artifactId>fastdfs-client-java</artifactId>
+ *             <version>1.29-SNAPSHOT</version>
+ *             <exclusions>
+ *                 <exclusion>
+ *                     <groupId>org.slf4j</groupId>
+ *                     <artifactId>slf4j-log4j12</artifactId>
+ *                 </exclusion>
+ *             </exclusions>
+ *         </dependency>
+ * 2、在resources 下 添加 fdfs_client.conf
+ *
+ */
 public class FastDFSClient {
 
     private StorageClient1 client;
@@ -31,10 +48,9 @@ public class FastDFSClient {
 
     /**
      * 文件上传
-     * <p>
-     * //     * @param file 文件
      *
-     * @return 上传成功返回id，失败返回null
+     * @param fileName 文件名称
+     * @return 上传成功返回fileId，失败返回null
      */
     public String upload(String fileName) {
         try {
@@ -52,6 +68,14 @@ public class FastDFSClient {
         return Splitter.on(".").trimResults().omitEmptyStrings().splitToList(fileName).get(1);
     }
 
+    /**
+     * 文件下载
+     *
+     * @param destDir  要下载到的目录
+     * @param fileId   要下载的文件id
+     * @param fileName 下载后文件的名称
+     * @return
+     */
     public File downloadFile(String destDir, String fileId, String fileName) {
         //wKgCFF8VPh6ARuQxAACTPzoN2uc34.jpeg group1/M00/00/01/wKgCFF8VPh6ARuQxAACTPzoN2uc34.jpeg
         String imageFullPath = destDir + fileName;
